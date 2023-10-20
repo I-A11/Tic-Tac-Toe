196de70e7ca60829a0c910b18b9ab574b4167236
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-10-20 19:41:49 +1100

    Create square component to use it in app.js.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
+import Square from "./component/Square";
+
 function App() {
   return (
     <div className="app">
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square last">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square last">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square bottom">7</button>
-        <button className="square bottom">8</button>
-        <button className="square last bottom">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </div>
   );
